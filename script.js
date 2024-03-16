7693285d05c06f43f54a936b0f73339589758ae2
RishabhSingh4872962 <109566428+RishabhSingh4872962@users.noreply.github.com>
2024-03-16 12:20:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // your code here
+const name=document.getElementById("name");
+const year=document.getElementById("year");
+const url=document.getElementById("url");
+
+function handleSubmit() {
+	let newUrl=`https://localhost:8080/`;
+	if (name.value && year.value) {
+		newUrl+=`?name=${name.value}&year=${year.value}`
+	}else if (name.value) {
+		newUrl+=`?name=${name.value}`
+	}else if (year.value) {
+		newUrl+=`?year=${year.value}`
+	} 
+	url.innerText=newUrl
+}
